4b931f85e01ccf95328d1da3327baf24fc4e2915
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-08-13 21:54:04 -0500

    Add files via upload

--- Hashing/hashing.py
+++ Hashing/hashing.py
@@ -1,1 +1,13 @@
-1
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 21:51:39 2020
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Hashing %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+@author: Wenqing Hu (Missouri S&T)
+
+"""
+
+# do Hash map
+
