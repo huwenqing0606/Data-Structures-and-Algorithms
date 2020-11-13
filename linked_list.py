d12de6d83844b2c9528cd28573d600dff7fbc065
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-11-13 17:42:23 -0600

    Add files via upload

--- linked_list.py
+++ linked_list.py
@@ -18,15 +18,35 @@ Includes:
 # build a node in a linked list
 class Node:
     
-    def __init__(self, context = None, pt_next = None):
-        self.context = context
-        self.next = pt_next
+    def __init__(self,
+                 node_id,
+                 node_context,
+                 next_node
+                 ):
+        self.id = node_id
+        self.context = node_context
+        self.next = next_node
     
     def show(self):
         print(self.context)
         return None
     
 # build a linked list
+class linked_list:
+    
+    def __init__(self, 
+                 number_nodes, 
+                 node_contexts,
+                 next_nodes
+                 ):
+        self.nodes = [Node(i, node_contexts[i], next_nodes[i]) for i in range(number_nodes)]
+        
+    def next_node_context(self, node_id):
+        return self.nodes[self.nodes[node_id].next].context
 
 
+if __name__=="__main__":
+    
+    linked_list = linked_list(3, ['a', 'b', 'c'], [1, 2, 0])
+    print(linked_list.next_node_context(0))
 
