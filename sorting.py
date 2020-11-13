ff9e9c9314af2759ac5f41e89a874623990b46f9
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-11-12 21:29:18 -0600

    Add files via upload

--- sorting.py
+++ sorting.py
@@ -3,6 +3,28 @@
 """
 Created on Thu Nov 12 14:36:09 2020
 
-@author: huwen
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Sorting Algorithms %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+An implementation of various sorting algoritms
+
+Includes: 
+    (1)     Quicksort
+    (2)     Heapsort
+    (3)     Shellsort
+    (4)     Merge sort
+    (5)     Introsort
+    (6)     Insertion sort
+    (7)     Selection sort
+    (8)     Bubble sort
+    (9)     Odd–even sort
+    (10)    Cocktail shaker sort
+    (11)    Cycle sort
+    (12)    Merge-insertion sort
+    (13)    Smoothsort
+    (14)    Timsort
+    (15)    Block sort
+
+@uthor: Wenqing Hu (Missouri S&T)
 """
+
 
